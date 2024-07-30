1c21d612ba38d8c45ddd320a735da69c7a544599
Bruno <bruno.pasquini@endava.com>
2024-07-30 17:11:59 -0300

    Environment variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { environment as env } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}} Bruno</div>`,
+  template: `
+  <div>Hello Bruno</div>
+  <div>environment:  {{environmentName}}</div>
+  <div>api url:  {{environmentName}}</div>
+  `,
 })
+
 export class AppComponent {
-  value = 'World';
+  constructor() {
+    if(!env.production)
+      console.log("Prod: "+env.production); // Logs false for development environment
+  }
+
+  title = 'app works!';
+  environmentName = env.environmentName;
+  apiurl = env.apiUrl;
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+    environmentName: "Prod",
+    apiUrl: 'http://my-prod-url'
 };
